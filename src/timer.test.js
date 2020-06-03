7bb19ded3e4a5ebefdcdb7de67ba9cfd5a3a1b36
Yura-by <s.older681@gmail.com>
2020-06-03 21:22:27 +0300

    Напишет тесты для timer

--- src/timer.test.js
+++ src/timer.test.js
@@ -1,12 +1,33 @@
 import Timer from './timer.js';
 
-it(`Timer currently gets arguments`, () => {
+it(`Timer currently tick timer`, () => {
   const onTimerEnd = jest.fn();
+
   const newTimer = new Timer(10, onTimerEnd);
+
   expect(newTimer).toEqual({
-    _totalTime: expect(10),
-    _onTimerEnd: expect.toEqual(onTimerEnd),
-    _lastTime: expect(10),
+    _totalTime: 10,
+    _onTimerEnd: onTimerEnd,
+    _lastTime: 10,
     tick: expect.any(Function)
   });
+
+  newTimer.tick();
+
+  expect(newTimer).toEqual({
+    _totalTime: 10,
+    _onTimerEnd: onTimerEnd,
+    _lastTime: 9,
+    tick: expect.any(Function)
+  });
+});
+
+it(`Timer currently gives time, calles callback when time ends`, () => {
+  const onTimerEnd = jest.fn();
+
+  const newTimer = new Timer(1, onTimerEnd);
+  expect(newTimer.getLastTime()).toBe(1);
+  newTimer.tick();
+  expect(newTimer.getLastTime()).toBe(0);
+  expect(onTimerEnd).toHaveBeenCalledTimes(1);
 });
